c0c1acc1a90cd5a2372a6e1a9baacc163b4d4551
Pinhui Lin <reevizy@gmail.com>
2019-05-15 02:15:22 +0800

    fix setup

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ setup(
     author='Rio Lin',
     author_email='pinhuilin86@gmail.com',
     url='https://github.com/riorz/wss_chat',
-    package=['chatroom'],
+    packages=['chatroom'],
     install_requires=['websockets', 'blessed'],
     entry_points={
         'console_scripts': [
-            'chatroom = chatroom.scripts.command_line:main',
+            'chatroom = chatroom.command_line:main',
         ],
     },
 )
